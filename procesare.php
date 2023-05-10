b53fb85d7db3a4bc8ff8d662592ae1ee08232e76
IoanaBulgariu <131755935+IoanaBulgariu@users.noreply.github.com>
2023-05-10 12:54:32 +0100

    c

--- procesare.php
+++ procesare.php
@@ -14,6 +14,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$municipio = $_POST["municipio"];
 	$url_info = $_POST["url_info"];
 	$url_maps = $_POST["url_maps"];
+
+	$error = false;
+
+	if ($nombre_lugar == "")
+	{
+		echo "<br>ERROR: The name place is empty";
+		$error = true;
+	}
   
 	// Verificam si incarcam imaginea
 	if(isset($_FILES['imagine'])) {
@@ -22,6 +30,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	  $folder = "imagini/";
 	  move_uploaded_file($tmp_imagine, $folder.$nume_imagine);
 	}
+	$linie = $nombre_lugar . ';' . $descripcion_lugar . ';' . $municipio . ';' . $url_info . ';' . $url_maps . ';' . $nume_imagine . "\n";
+
+	$fisier_csv = 'date.csv';
+	$handle = fopen($fisier_csv, 'a');
+	fwrite($handle, $linie);
+	fclose($handle);
+	if ($error == false)
+	{
   
 	// Afisez valorile pe pagina
 	echo "<p>Nombre del lugar: $nombre_lugar</p>";
@@ -29,10 +45,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	echo "<p>Municipio: $municipio</p>";
 	echo "<p>URL con Mas Informacion: $url_info</p>";
 	echo "<p>URL con enlace a Google Maps: $url_maps</p>";
+	echo "<li>Datele au fost salvate în fișierul " . $fisier_csv;
 	if(isset($nume_imagine)) {
 	  echo "<p><img src='imagini/$nume_imagine'></p>";
 	}
   }
+}
 
 ?>
 
